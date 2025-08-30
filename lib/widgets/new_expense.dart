49a0abb218b53b19f545403f73a761e53a59c6e2
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2025-08-30 16:27:32 +0700

    TextFiled

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,15 +10,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: const Text('Save Expense'),
+              ),
+            ],
           )
         ],
       ),
