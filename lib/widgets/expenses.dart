3b510e20e7833add9ac4910ecf2de3766504b8b7
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2025-08-30 10:43:45 +0700

    add model sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,14 +28,21 @@ class _ExpensesState extends State<Expenses> {
     )
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Model bottom sheet')
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter ExpenseTracker'),
+        title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
